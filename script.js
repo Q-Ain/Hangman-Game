f5b41546cc4a51d284f521221e28bfb959c1fca5
Q-Ain <65014918+Q-Ain@users.noreply.github.com>
2022-05-26 04:43:55 +0800

    Fix win page + clean code + add comments

--- script.js
+++ script.js
@@ -5,15 +5,13 @@ window.onload = function () {
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
   var categories;         // Array of topics
-  var chosenCategory;     // Selected catagory
-  // var getHint;          // Word getHint
+  var chosenCategory;     // Selected category
   var word;              // Selected word
   var guess;             // guess
   var guesses = [];      // Stored guesses
   var lives;             // Lives
   var counter;           // Count correct guesses
   var space;              // Number of spaces in word '-'
-  // added
   var numRounds;
   var correctWord;
 
@@ -21,7 +19,7 @@ window.onload = function () {
   var showRound = document.getElementById("myrounds");
   var showClue = document.getElementById("clue");
 
-  // create alphabet ul
+  // Create alphabet ul
   var buttons = function () {
     myButtons = document.getElementById('buttons');
     letters = document.createElement('ul');
@@ -38,14 +36,14 @@ window.onload = function () {
   }
 
 
-  // Select Catagory
+  // Select category
   var selectCat = function () {
     if (chosenCategory === categories[0]) {
-      catagoryName.innerHTML = "Category: Flowers";
+      categoryName.innerHTML = "Category: Flowers";
     } else if (chosenCategory === categories[1]) {
-      catagoryName.innerHTML = "Category: Sports";
+      categoryName.innerHTML = "Category: Sports";
     } else if (chosenCategory === categories[2]) {
-      catagoryName.innerHTML = "Category: Colours";
+      categoryName.innerHTML = "Category: Colours";
     }
   }
 
@@ -71,10 +69,10 @@ window.onload = function () {
     }
   }
 
-  // Show lives
+  // Display rounds
   comments = function () {
     if (lives < 1) {
-      showRound.innerHTML = "Game Over";
+      showRound.innerHTML = "Game Over. Press reset to play again...";
     }
 
     for (var i = 0; i < guesses.length; i++) {
@@ -120,7 +118,6 @@ window.onload = function () {
           selectCat();
           canvas();
         }
-        // console.log(numRounds);
     }
 
     correctWord = false; 
@@ -131,7 +128,6 @@ window.onload = function () {
     var drawMe = lives;
     drawArray[drawMe]();
   }
-
 
   // Hangman
   canvas = function () {
@@ -252,14 +248,14 @@ window.onload = function () {
   hint.onclick = function () {
 
     hints = [
-      ["has seeds that can be eaten", "fifty of them signifies unconditional love", "a song by Alan Walker", "a character in Micky Mouse Clubhouse", "an MRT station in Singapore"],
+      ["has seeds that can be eaten", "fifty of them signifies unconditional love", "a song by Alan Walker", "a character in Mickey Mouse Clubhouse", "an MRT station in Singapore"],
       ["Haikyu!!", "Kuroko no Basuke", "Birdie Wing", "Free!", "Rafael Nadal"],
       ["colour in a rainbow", "water + sea", "part of a band's name", "condiment", "name of a city in Pokemon"]
     ];
 
-    var catagoryIndex = categories.indexOf(chosenCategory);
+    var categoryIndex = categories.indexOf(chosenCategory);
     var hintIndex = chosenCategory.indexOf(word);
-    showClue.innerHTML = "Hint: " + hints[catagoryIndex][hintIndex];
+    showClue.innerHTML = "Hint: " + hints[categoryIndex][hintIndex];
   };
 
   // Reset
@@ -271,7 +267,7 @@ window.onload = function () {
     play();
   }
 
-  // ? How To Play
+  // Pop-up for ? How To Play 
   // Get the modal
   var modal = document.getElementById("myModal");
 
@@ -289,131 +285,9 @@ window.onload = function () {
       modal.style.display = "none";
     }
   }
-
-  // firework = function(){
-  //   window.addEventListener("resize", resizeCanvas, false);
-  //       window.addEventListener("DOMContentLoaded", onLoad, false);
-        
-  //       window.requestAnimationFrame = 
-  //           window.requestAnimationFrame       || 
-  //           window.webkitRequestAnimationFrame || 
-  //           window.mozRequestAnimationFrame    || 
-  //           window.oRequestAnimationFrame      || 
-  //           window.msRequestAnimationFrame     || 
-  //           function (callback) {
-  //               window.setTimeout(callback, 1000/60);
-  //           };
-        
-  //       var canvas, ctx, w, h, particles = [], probability = 0.04,
-  //           xPoint, yPoint;
-        
-  //       function onLoad() {
-  //           canvas = document.getElementById("canvas");
-  //           ctx = canvas.getContext("2d");
-  //           resizeCanvas();
-            
-  //           window.requestAnimationFrame(updateWorld);
-  //       } 
-        
-  //       function resizeCanvas() {
-  //           if (!!canvas) {
-  //               w = canvas.width = window.innerWidth;
-  //               h = canvas.height = window.innerHeight;
-  //           }
-  //       } 
-        
-  //       function updateWorld() {
-  //           update();
-  //           paint();
-  //           window.requestAnimationFrame(updateWorld);
-  //       } 
-        
-  //       function update() {
-  //           if (particles.length < 500 && Math.random() < probability) {
-  //               createFirework();
-  //           }
-  //           var alive = [];
-  //           for (var i=0; i<particles.length; i++) {
-  //               if (particles[i].move()) {
-  //                   alive.push(particles[i]);
-  //               }
-  //           }
-  //           particles = alive;
-  //       } 
-        
-  //       function paint() {
-  //           ctx.globalCompositeOperation = 'source-over';
-  //           ctx.fillStyle = "rgba(0,0,0,0.2)";
-  //           ctx.fillRect(0, 0, w, h);
-  //           ctx.globalCompositeOperation = 'lighter';
-  //           for (var i=0; i<particles.length; i++) {
-  //               particles[i].draw(ctx);
-  //           }
-  //       } 
-        
-  //       function createFirework() {
-  //           xPoint = Math.random()*(w-200)+100;
-  //           yPoint = Math.random()*(h-200)+100;
-  //           var nFire = Math.random()*50+100;
-  //           var c = "rgb("+(~~(Math.random()*200+55))+","
-  //                +(~~(Math.random()*200+55))+","+(~~(Math.random()*200+55))+")";
-  //           for (var i=0; i<nFire; i++) {
-  //               var particle = new Particle();
-  //               particle.color = c;
-  //               var vy = Math.sqrt(25-particle.vx*particle.vx);
-  //               if (Math.abs(particle.vy) > vy) {
-  //                   particle.vy = particle.vy>0 ? vy: -vy;
-  //               }
-  //               particles.push(particle);
-  //           }
-  //       } 
-        
-  //       function Particle() {
-  //           this.w = this.h = Math.random()*4+1;
-            
-  //           this.x = xPoint-this.w/2;
-  //           this.y = yPoint-this.h/2;
-            
-  //           this.vx = (Math.random()-0.5)*10;
-  //           this.vy = (Math.random()-0.5)*10;
-            
-  //           this.alpha = Math.random()*.5+.5;
-            
-  //           this.color;
-  //       } 
-        
-  //       Particle.prototype = {
-  //           gravity: 0.05,
-  //           move: function () {
-  //               this.x += this.vx;
-  //               this.vy += this.gravity;
-  //               this.y += this.vy;
-  //               this.alpha -= 0.01;
-  //               if (this.x <= -this.w || this.x >= screen.width ||
-  //                   this.y >= screen.height ||
-  //                   this.alpha <= 0) {
-  //                       return false;
-  //               }
-  //               return true;
-  //           },
-  //           draw: function (c) {
-  //               c.save();
-  //               c.beginPath();
-                
-  //               c.translate(this.x+this.w/2, this.y+this.h/2);
-  //               c.arc(0, 0, this.w, 0, Math.PI*2);
-  //               c.fillStyle = this.color;
-  //               c.globalAlpha = this.alpha;
-                
-  //               c.closePath();
-  //               c.fill();
-  //               c.restore();
-  //           }
-  //       } 
-  // }
 }
 
-// Hide answer from being displayed in console
+// Hide answer from being displayed in inspect console
 console.log = function () { }
 
 
